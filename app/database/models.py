60a5171703f85040957100b879c817f014a33c88
BGDSR <66807066+BGDSR@users.noreply.github.com>
2025-04-19 20:57:57 +0300

    Добавил psycopg2-binary в зависимости

--- app/database/models.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import Column, Integer, BigInteger, DateTime
-from sqlalchemy.sql import func
-from app.database.db import Base
-
-class User(Base):
-    __tablename__ = "users"
-    id = Column(Integer, primary_key=True)
-    tg_id = Column(BigInteger, unique=True, index=True)
-    joined_at = Column(DateTime(timezone=True), server_default=func.now())
