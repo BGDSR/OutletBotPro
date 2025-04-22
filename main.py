f7267a8e79d88768cdce9e93a472172e6f5813be
BGDSR <66807066+BGDSR@users.noreply.github.com>
2025-04-22 18:07:11 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
-from aiogram import Bot, Dispatcher, types
-from aiogram.types import Message
-from aiogram.enums import ParseMode
-from aiogram.fsm.storage.memory import MemoryStorage
-from aiogram import Router
-from app.handlers import user, admin
 import asyncio
 import os
+from aiogram import Bot, Dispatcher
+from aiogram.enums import ParseMode
+from aiogram.fsm.storage.memory import MemoryStorage
 
+from app.handlers import user, admin
 from app.database.db import create_db
 
 TOKEN = os.getenv("BOT_TOKEN")
+
 bot = Bot(token=TOKEN, parse_mode=ParseMode.HTML)
 dp = Dispatcher(storage=MemoryStorage())
 
+# Подключаем роутеры
 dp.include_router(user.router)
 dp.include_router(admin.router)
 
