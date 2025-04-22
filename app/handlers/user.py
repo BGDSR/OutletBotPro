d54845901da639de1c9590350f84c17f62a5d0d1
BGDSR <66807066+BGDSR@users.noreply.github.com>
2025-04-22 18:16:12 +0300

    Update user.py

--- app/handlers/user.py
+++ app/handlers/user.py
@@ -1,16 +1,21 @@
 from aiogram import Router, types
-from aiogram.filters import Command
 from app.database.db import AsyncSessionLocal
 from app.database.models import User
+from sqlalchemy import select
 
 router = Router()
 
 @router.message(Command("start"))
 async def start_cmd(message: types.Message):
     async with AsyncSessionLocal() as session:
-        user = await session.get(User, message.from_user.id)
+        result = await session.execute(
+            select(User).where(User.tg_id == message.from_user.id)
+        )
+        user = result.scalar_one_or_none()
+
         if not user:
             user = User(tg_id=message.from_user.id)
             session.add(user)
             await session.commit()
+
     await message.answer("Добро пожаловать в OutletBot!")
