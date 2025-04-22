7ff974d41b6b0b30856999a5eae3a96b87d059d2
BGDSR <66807066+BGDSR@users.noreply.github.com>
2025-04-22 20:46:36 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,24 +1,31 @@
-from aiogram import Bot, Dispatcher, types
-from aiogram.types import Message
+from aiogram import Bot, Dispatcher
 from aiogram.enums import ParseMode
-from aiogram.fsm.storage.memory import MemoryStorage
-from aiogram import Router
+from aiogram.fsm.storage.redis import RedisStorage  # Изменено!
 from app.handlers import user, admin
-import asyncio
-import os
-
 from app.database.db import create_db
+import asyncio
+import logging
+from app.config import BOT_TOKEN
 
-TOKEN = os.getenv("BOT_TOKEN")
-bot = Bot(token=TOKEN, parse_mode=ParseMode.HTML)
-dp = Dispatcher(storage=MemoryStorage())
-
-dp.include_router(user.router)
-dp.include_router(admin.router)
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 async def main():
-    await create_db()
-    await dp.start_polling(bot)
+    try:
+        # Инициализация Redis для FSM и кеша
+        storage = RedisStorage.from_url("redis://localhost:6379/0")
+        
+        bot = Bot(token=BOT_TOKEN, parse_mode=ParseMode.HTML)
+        dp = Dispatcher(storage=storage)
+
+        dp.include_router(user.router)
+        dp.include_router(admin.router)
+
+        await create_db()
+        logger.info("Бот запущен")
+        await dp.start_polling(bot)
+    except Exception as e:
+        logger.critical(f"Ошибка запуска: {e}")
 
 if __name__ == "__main__":
     asyncio.run(main())
