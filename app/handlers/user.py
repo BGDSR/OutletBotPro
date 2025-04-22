b3c4a7593cd2664a13244041f20044de0e63d3cc
BGDSR <66807066+BGDSR@users.noreply.github.com>
2025-04-22 18:02:16 +0300

    Update user.py

--- app/handlers/user.py
+++ app/handlers/user.py
@@ -1,13 +1,14 @@
 from aiogram import Router, types
+from aiogram.filters import Command
 from app.database.db import AsyncSessionLocal
 from app.database.models import User
 
 router = Router()
 
-@router.message(commands=["start"])
+@router.message(Command("start"))
 async def start_cmd(message: types.Message):
     async with AsyncSessionLocal() as session:
-        user = await session.get(User, {"tg_id": message.from_user.id})
+        user = await session.get(User, message.from_user.id)
         if not user:
             user = User(tg_id=message.from_user.id)
             session.add(user)
