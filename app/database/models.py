a6f23e46ed66bae51b5c2b9fa9818f981f53d662
BGDSR <66807066+BGDSR@users.noreply.github.com>
2025-04-23 01:09:58 +0300

    Update models.py

--- app/database/models.py
+++ app/database/models.py
@@ -1,10 +1,14 @@
-from sqlalchemy import Column, Integer, BigInteger, DateTime, Boolean
+from sqlalchemy import Column, Integer, BigInteger, String, DateTime, Boolean
 from sqlalchemy.sql import func
 from app.database.db import Base
 
 class User(Base):
     __tablename__ = "users"
+
     id = Column(Integer, primary_key=True)
-    tg_id = Column(BigInteger, unique=True, index=True)
+    tg_id = Column(BigInteger, unique=True, index=True, nullable=False)
+    username = Column(String, nullable=True)
+    first_name = Column(String, nullable=True)
+    last_name = Column(String, nullable=True)
     joined_at = Column(DateTime(timezone=True), server_default=func.now())
-    is_active = Column(Boolean, default=True)  # Для управления рассылками
+    is_active = Column(Boolean, default=True)
