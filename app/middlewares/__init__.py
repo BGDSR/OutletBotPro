df8ed33499fe740e5267ce06156ec55bb76e8890
BGDSR <66807066+BGDSR@users.noreply.github.com>
2025-04-25 00:11:17 +0300

    Create __init__.py

---
+++ app/middlewares/__init__.py
@@ -0,0 +1,7 @@
+from aiogram import Dispatcher
+
+def register_middlewares(dp: Dispatcher):
+    """Регистрация всех middleware"""
+    # Пример:
+    # dp.update.outer_middleware(SomeMiddleware())
+    pass
